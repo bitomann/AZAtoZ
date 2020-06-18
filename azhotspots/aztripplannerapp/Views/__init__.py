4125458e77bce2fe7baa45b76f4dabf1ea9c67e5
Bito Mann <bito@bitomann.com>
2020-06-18 10:53:34 -0500

    random commit

---
+++ azhotspots/aztripplannerapp/Views/__init__.py
@@ -0,0 +1,7 @@
+from .home import home
+from .auth.logout import logout_user
+from .books.list import book_list
+from .librarians.list import librarian_list
+from .libraries.list import library_list
+from .books.form import book_form, book_edit_form
+from .books.details import book_details
---
+++ azhotspots/aztripplannerapp/Views/connection.py
@@ -0,0 +1,2 @@
+class Connection:
+    db_path = "/Users/Bito1/workspace/C38/AZAtoZ/azhotspots/db.sqlite3"
