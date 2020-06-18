d167779d62299a7d40b80594411ee42956a7f5f1
Bito Mann <bito@bitomann.com>
2020-06-18 12:17:11 -0500

    rc

--- azhotspots/aztripplannerapp/Views/userhotspots/form.py
+++ azhotspots/aztripplannerapp/Views/userhotspots/form.py
@@ -1,1 +1,41 @@
 # hot spot first
+import sqlite3
+from django.shortcuts import render
+from django.contrib.auth.decorators import login_required
+from aztripplannerapp.models import HotSpot, UserHotSpots, Itinerary
+
+
+@login_required
+def get_hotspots():
+    return HotSpot.objects.all()
+
+@login_required
+def get_userhotspots():
+    return UserHotSpots.objects.all()
+
+
+@login_required
+def userhotspot_form(request):
+    if request.method == 'GET':
+        libraries = get_libraries()
+        template = 'userhotspots/form.html'
+        context = {
+            'all_libraries': libraries
+        }
+
+        return render(request, template, context)
+    
+@login_required
+def userhotspot_edit_form(request, userhotspot_id):
+
+    if request.method == 'GET':
+        userhotspot = get_userhotspot(userhotspot_id)
+        libraries = get_libraries()
+
+        template = 'userhotspots/form.html'
+        context = {
+            'userhotspot': userhotspot,
+            'all_libraries': libraries
+        }
+
+        return render(request, template, context)
