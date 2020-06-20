c713e8a3ea23511b08d4ef1abfc66905e2c42335
Bito Mann <bito@bitomann.com>
2020-06-20 15:36:13 -0500

    rc

--- azhotspots/aztripplannerapp/Views/__init__.py
+++ azhotspots/aztripplannerapp/Views/__init__.py
@@ -5,4 +5,5 @@ from .userhotspots.list import userhotspot_list
 from .userhotspots.form import userhotspot_form
 from .itineraries.list import itinerary_list
 from .itineraries.details import itinerary_details
+from .itineraries.form import itinerary_form
 from .hotspots.details import hotspot_details
