68602a20057d792ae2b816833b7aa10bc1cb9ac9
Bito Mann <bito@bitomann.com>
2020-06-22 10:51:53 -0500

    rc

--- azhotspots/aztripplannerapp/Views/itineraries/form.py
+++ azhotspots/aztripplannerapp/Views/itineraries/form.py
@@ -1,7 +1,7 @@
 import sqlite3
 from django.shortcuts import render
 from django.contrib.auth.decorators import login_required
-from azhotspots.aztripplannerapp.models import HotSpot, UserHotSpots, Itinerary
+from aztripplannerapp.models import HotSpot, UserHotSpots, Itinerary
 
 
 @login_required
